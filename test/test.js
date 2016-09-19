362c5272dda97a50aba3556544fd08e2514025ac
inikulin <ifaaan@gmail.com>
2016-09-19 15:09:55 +0300

    Tests for fail scenarios

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 var expect         = require('chai').expect;
 var vinylFile      = require('vinyl-file');
+var promisifyEvent = require('promisify-event');
 var gulpTestCafe   = require('../');
 
 function createReportOutStream () {
@@ -13,7 +14,7 @@ function createReportOutStream () {
 }
 
 
-it('Should run tests', function (done) {
+it('Should run tests', function () {
     var reportStream = createReportOutStream();
 
     var ps = gulpTestCafe({
@@ -21,16 +22,73 @@ it('Should run tests', function (done) {
         reportOutStream: reportStream
     });
 
-    ps
-        .once('error', done)
-        .once('done', function () {
+
+    var resultsPromise = Promise.race([
+        promisifyEvent(ps, 'error'),
+
+        promisifyEvent(ps, 'done').then(function () {
             expect(reportStream.data).contains('Chrome');
             expect(reportStream.data).contains('Firefox');
             expect(reportStream.data).contains('3 passed');
-            done();
-        });
+        })
+    ]);
 
     ps.write(vinylFile.readSync('test/fixtures/passing1.js'));
     ps.write(vinylFile.readSync('test/fixtures/passing2.js'));
     ps.end();
+
+    return resultsPromise;
+});
+
+it('Should fail if tests fail', function () {
+    var reportStream = createReportOutStream();
+
+    var ps = gulpTestCafe({
+        browsers:        ['chrome'],
+        reportOutStream: reportStream
+    });
+
+    var resultsPromise = Promise.race([
+        promisifyEvent(ps, 'error').catch(function (err) {
+            expect(err.message).eql('2 test(s) failed.');
+            expect(reportStream.data).contains('Chrome');
+            expect(reportStream.data).contains('2/4 failed');
+        }),
+
+        promisifyEvent(ps, 'done').then(function () {
+            throw new Error('Error expected');
+        })
+    ]);
+
+    ps.write(vinylFile.readSync('test/fixtures/passing1.js'));
+    ps.write(vinylFile.readSync('test/fixtures/failing.js'));
+    ps.end();
+
+    return resultsPromise;
+});
+
+it('Should fail if configuration is incorrect', function () {
+    var reportStream = createReportOutStream();
+
+    var ps = gulpTestCafe({
+        browsers:        ['chrome'],
+        reporter:        'unknown',
+        reportOutStream: reportStream
+    });
+
+    var resultsPromise = Promise.race([
+        promisifyEvent(ps, 'error').catch(function (err) {
+            expect(err.message).eql('The provided "unknown" reporter does not exist. Check that you have specified the report format correctly.');
+            expect(reportStream.data).eql('');
+        }),
+
+        promisifyEvent(ps, 'done').then(function () {
+            throw new Error('Error expected');
+        })
+    ]);
+
+    ps.write(vinylFile.readSync('test/fixtures/passing1.js'));
+    ps.end();
+
+    return resultsPromise;
 });
